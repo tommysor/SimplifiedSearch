12235d8816661c641913cb33361f7bd52c7329d6
Tommy Sørbråten <tommysor@gmail.com>
2021-12-30 14:09:59 +0100

    Remove half done code intended for future public exposure.

--- src/SimplifiedSearch/SimplifiedSearchConfiguration.cs
+++ src/SimplifiedSearch/SimplifiedSearchConfiguration.cs
@@ -4,21 +4,7 @@ using System.Text;
 
 namespace SimplifiedSearch
 {
-    //todo doc public record SimplifiedSearchConfiguration
-    /// <summary>
-    /// 
-    /// </summary>
     internal record SimplifiedSearchConfiguration
     {
-        /// <summary>
-        /// Summary:
-        ///     Specifies how different strings can be and still count as a match.
-        ///     Value of 1 means search string "aaaa" will match "aaab", but not "aabb".
-        /// </summary>
-        /// <remarks>
-        /// Alternative names:
-        ///     EditDistance, LevenshteinDistance.
-        /// </remarks>
-        public uint FuzzySearchMaxDifferences { get; set; } = 2;
     }
 }
