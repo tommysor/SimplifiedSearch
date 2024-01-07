4e11bd92b5be8b612739dbd299236ae4dba1c9e7
Tommy Sørbråten <tommysor@gmail.com>
2024-01-07 14:39:20 +0000

    Remove useless allocations

--- src/SimplifiedSearch/SearchPipelines/TokenPipelines/Components/AsciiFoldingFilter.cs
+++ src/SimplifiedSearch/SearchPipelines/TokenPipelines/Components/AsciiFoldingFilter.cs
@@ -11,14 +11,12 @@ namespace SimplifiedSearch.SearchPipelines.TokenPipelines.Components
         public Task<string[]> RunAsync(params string[] value)
         {
             var len = value.Length;
-            var results = new string[len];
             for (var i = 0; i < len; i++)
             {
-                var asciiString = value[i].Unidecode();
-                results[i] = asciiString;
+                value[i] = value[i].Unidecode();
             }
 
-            return Task.FromResult(results);
+            return Task.FromResult(value);
         }
     }
 }
